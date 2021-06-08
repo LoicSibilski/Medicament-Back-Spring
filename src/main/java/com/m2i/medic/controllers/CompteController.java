f12679b2d16c03ae0b098cb3e00f72c34ec7dfd0
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-08 16:09:06 +0200

    (feat) ajout : méthode pour utiliser le service pour récupérer tous les comptes

--- src/main/java/com/m2i/medic/controllers/CompteController.java
+++ src/main/java/com/m2i/medic/controllers/CompteController.java
@@ -1,6 +1,9 @@
 package com.m2i.medic.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.m2i.medic.dtos.CompteDTO;
 import com.m2i.medic.dtos.CreationNouveauCompteDTO;
+import com.m2i.medic.dtos.SimpleCompteDTO;
 import com.m2i.medic.services.CompteService;
 
 /**
@@ -30,5 +34,14 @@ public class CompteController {
 	@PostMapping("")
 	public CompteDTO creationNouveauCompte(@RequestBody CreationNouveauCompteDTO dto) {
 		return this.service.creationNouveauCompte(dto);
+	}
+	
+	/**
+	 * Cette méthode permet de retourner un service pour récupérer tous les comptes
+	 * @return List<SimpleCompteDTO>
+	 */
+	@GetMapping("")
+	public List<SimpleCompteDTO> recupérerTousLesComptes(){
+		return this.service.recupererTousLesComptes();
 	}
 }

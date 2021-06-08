fa2ab8c34e384aa8627a4152e8885b9621858980
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-08 11:20:00 +0200

    (feat) ajout : méthode pour récupérer un compte à partir d'un email

--- src/main/java/com/m2i/medic/repositories/CompteRepository.java
+++ src/main/java/com/m2i/medic/repositories/CompteRepository.java
@@ -2,6 +2,7 @@ package com.m2i.medic.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.m2i.medic.dtos.CreationNouveauCompteDTO;
 import com.m2i.medic.models.Compte;
 
 /**
@@ -11,4 +12,11 @@ import com.m2i.medic.models.Compte;
  */
 public interface CompteRepository extends MongoRepository<Compte, String> {
 
+	/**
+	 * Cette méthode permet de récupérer un compte à partir d'une adresse email
+	 * @param email
+	 * @return CompteDTO
+	 */
+	public CreationNouveauCompteDTO findByEmail(String email);
+	
 }

b018e43f4df8d24ee43f322472b13ba1d18d02f2
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-09 11:40:24 +0200

    (feat) mise à jour : documentation

--- src/main/java/com/m2i/medic/dtos/SimpleCompteDTO.java
+++ src/main/java/com/m2i/medic/dtos/SimpleCompteDTO.java
@@ -3,6 +3,11 @@ package com.m2i.medic.dtos;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Cette classe représente un DTO d'un compte avec des attributs en moins
+ * @author fabien
+ *
+ */
 @Data
 @NoArgsConstructor
 public class SimpleCompteDTO {

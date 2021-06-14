726b73252363f1e4c5ff2fe7576deeb45eba72e7
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-14 09:53:23 +0200

    (feat) mise à jour : valeur de retour modifié

--- src/main/java/com/m2i/medic/repositories/CompteRepository.java
+++ src/main/java/com/m2i/medic/repositories/CompteRepository.java
@@ -1,8 +1,9 @@
 package com.m2i.medic.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.m2i.medic.dtos.InscriptionDTO;
 import com.m2i.medic.models.Compte;
 
 /**
@@ -17,6 +18,6 @@ public interface CompteRepository extends MongoRepository<Compte, String> {
 	 * @param une adresse email
 	 * @return un compte
 	 */
-	public InscriptionDTO findByEmail(String email);
+	public Optional<Compte> findByEmail(String email);
 	
 }

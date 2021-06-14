ea9c75f70636aa1e799a372aaff2881530a1a20e
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-15 01:32:29 +0200

    (feat) mise à jour : méthodes verifier attributs et compte existe

--- src/main/java/com/m2i/medic/services/implementations/ModificateurCompteServiceImpl.java
+++ src/main/java/com/m2i/medic/services/implementations/ModificateurCompteServiceImpl.java
@@ -24,17 +24,15 @@ public class ModificateurCompteServiceImpl implements ModificateurCompteService 
 		this.mapper = mapper;
 		this.repository = repository;
 	}
-
-	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^(.+)@(\\S+)$");
-	
-	public static final Pattern VALID_MOTDEPASSE_ADDRESS_REGEX = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
-	
 	
 	@Override
 	public CompteDTO creerNouveauCompte(InscriptionDTO nouveauCompte) {
-		verifierCreationCompte(nouveauCompte);
+		String email = nouveauCompte.getEmail();
+		String motDePasse = nouveauCompte.getMotDePasse();
+		verifierCompteExiste(email);
+		verifierAttributsCompte(email, motDePasse);
 		Compte compte = this.mapper.convertValue(nouveauCompte, Compte.class);
-		compte.setMotDePasse(Base64.encode(nouveauCompte.getMotDePasse().getBytes())); // modifier le chiffrement
+		compte.setMotDePasse(Base64.encode(motDePasse.getBytes())); // modifier le chiffrement
 		compte.setDateCreation(LocalDateTime.now());
 		compte.setDateMisJour(LocalDateTime.now());
 		Compte compteSauvegarde = this.repository.save(compte);
@@ -45,11 +43,9 @@ public class ModificateurCompteServiceImpl implements ModificateurCompteService 
 	public void modifierEmailMotDePasse(CompteDTO compte) {		
 		String email = compte.getEmail();
 		String motDePasse = compte.getMotDePasse();
-		verifierEmailExiste(email);
-		verifierEmailFormat(email);
-		verifierMotDePasseFormat(motDePasse);
+		verifierCompteExiste(email);
+		verifierAttributsCompte(email, motDePasse);
 		Compte compteModifie = this.mapper.convertValue(compte, Compte.class);
-		compteModifie.setEmail(email);
 		compteModifie.setMotDePasse(Base64.encode(motDePasse.getBytes()));
 		compteModifie.setDateMisJour(LocalDateTime.now());
 		this.repository.save(compteModifie);
@@ -72,78 +68,34 @@ public class ModificateurCompteServiceImpl implements ModificateurCompteService 
 	 * Cette méthode permet de vérifier si le compte est conforme
 	 * @param un compte
 	 */
-	private void verifierCreationCompte(InscriptionDTO compte) {
-		verifierEmailExiste(compte);
-		verifierEmailFormat(compte);
-		verifierMotDePasseFormat(compte);
+	private void verifierAttributsCompte(String email, String motDePasse) {
+		Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^(.+)@(\\S+)$");
+		Pattern VALID_MOTDEPASSE_ADDRESS_REGEX = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
+		verifierAttributFormat(email, VALID_EMAIL_ADDRESS_REGEX);
+		verifierAttributFormat(motDePasse, VALID_MOTDEPASSE_ADDRESS_REGEX);
 	}
 	
 	/**
-	 * Cette méthode permet de vérifier si l'email existe en base de données
+	 * Cette méthode permet de vérifier si un compte existe à partir d'une adresse email
 	 * @param un compte
 	 */
-	private void verifierEmailExiste(InscriptionDTO compte) {
-		InscriptionDTO compteRecupere = this.repository.findByEmail(compte.getEmail());
+	private void verifierCompteExiste(String email) {
+		InscriptionDTO compteRecupere = this.repository.findByEmail(email);
 		if(compteRecupere != null) {			
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "L'email existe déjà");
 		}
 	}
 	
-	/**
-	 * Cette méthode permet de vérifier si l'email existe en base de données
-	 * @param un compte
-	 */
-	private void verifierEmailExiste(String email) {
-		InscriptionDTO compteRecupere = this.repository.findByEmail(email);
-		if(compteRecupere == null) {			
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "L'email existe déjà");
-		}
-	}
 	
 	/**
-	 * Cette méthode permet de vérifier si l'email correspond au bon format (regex)
+	 * Cette méthode permet de vérifier si l'attribut correspond au bon format (regex)
 	 * @param un compte
 	 */
-	private void verifierEmailFormat(InscriptionDTO compte) {
-		String email = compte.getEmail();
-        boolean emailValide = VALID_EMAIL_ADDRESS_REGEX.matcher(email).find();
-        if(!emailValide) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "L'email ne respecte pas le bon format");
+	private void verifierAttributFormat(String attribut, Pattern pattern) {
+        boolean attributValide = pattern.matcher(attribut).find();
+        if(!attributValide) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "L'attribut ne respecte pas le bon format");
         }
 	}
 	
-	/**
-	 * Cette méthode permet de vérifier si l'email correspond au bon format (regex)
-	 * @param un compte
-	 */
-	private void verifierEmailFormat(String email) {
-        boolean emailValide = VALID_EMAIL_ADDRESS_REGEX.matcher(email).find();
-        if(!emailValide) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "L'email ne respecte pas le bon format");
-        }
-	}
-	
-	/**
-	 * Cette méthode permet de vérifier si le mot de passe correspond au bon format (regex)
-	 * @param un compte
-	 */
-	private void verifierMotDePasseFormat(InscriptionDTO compte) {
-		String motDePasse = compte.getMotDePasse();
-        boolean motDePasseValide = VALID_MOTDEPASSE_ADDRESS_REGEX.matcher(motDePasse).find();
-        if(!motDePasseValide) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Le mot de passe ne respecte pas le bon format");
-        }
-	}
-	
-	/**
-	 * Cette méthode permet de vérifier si le mot de passe correspond au bon format (regex)
-	 * @param un compte
-	 */
-	private void verifierMotDePasseFormat(String motDePasse) {
-        boolean motDePasseValide = VALID_MOTDEPASSE_ADDRESS_REGEX.matcher(motDePasse).find();
-        if(!motDePasseValide) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Le mot de passe ne respecte pas le bon format");
-        }
-	}
-
 }

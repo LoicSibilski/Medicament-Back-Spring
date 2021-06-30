aec5f58f8c7cde1b009acdf8ea4d80958843b936
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-30 21:45:32 +0200

    feat(compte repositories): ajout de la méthode pour vérifier si un compte existe

---
+++ src/main/java/com/m2i/medic/compte/repositories/ModificateurCompteRepository.java
@@ -0,0 +1,21 @@
+package com.m2i.medic.compte.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.m2i.medic.compte.entities.Compte;
+
+/**
+ * Cette classe représente le repository modificateur d'un compte
+ * @author fabien
+ *
+ */
+public interface ModificateurCompteRepository extends MongoRepository<Compte, String> {
+
+	/**
+	 * Cette méthode permet de récupérer une valeur vrai ou fausse, si un pseudo ou email existe
+	 * @param emailOrPseudo
+	 * @return vrai ou faux
+	 */
+	public Boolean findByEmailOrPseudo(String emailOrPseudo);
+	
+}

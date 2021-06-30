9e57b1052c74fdf16f7e5cf2261bfedb12197f29
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-30 21:35:54 +0200

    feat(compte dtos): ajout des attributs pour le dto de desactivation d'un compte

---
+++ src/main/java/com/m2i/medic/compte/dtos/DesactivationCompteDTO.java
@@ -0,0 +1,17 @@
+package com.m2i.medic.compte.dtos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Cette classe représente un DTO de la desactivation d'un compte
+ * @author fabien
+ *
+ */
+@Data
+@NoArgsConstructor
+public class DesactivationCompteDTO {
+
+	private String id;
+	private Boolean etat;
+}

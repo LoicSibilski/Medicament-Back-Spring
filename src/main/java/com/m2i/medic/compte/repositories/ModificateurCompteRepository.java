4a6853f9296dc748a8cff1b740e8770f06d2dd8b
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-02 04:05:32 +0200

    feat(CompteRepository): mise à jour des méthodes

--- src/main/java/com/m2i/medic/compte/repositories/ModificateurCompteRepository.java
+++ src/main/java/com/m2i/medic/compte/repositories/ModificateurCompteRepository.java
@@ -1,7 +1,11 @@
 package com.m2i.medic.compte.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.m2i.medic.compte.dtos.InscriptionDTO;
 import com.m2i.medic.compte.entities.Compte;
 
 /**
@@ -12,16 +16,16 @@ import com.m2i.medic.compte.entities.Compte;
 public interface ModificateurCompteRepository extends MongoRepository<Compte, String> {
 
 	/**
-	 * Cette méthode permet de récupérer une valeur vrai ou fausse, si l'email de l'utilisateur existe
+	 * Cette méthode permet de récupérer une liste d'email si l'email de l'utilisateur existe
 	 * @param email
 	 * @return vrai ou faux
 	 */
-	public boolean findByEmail(String email);
+	public String findByEmail(String email);
 	
 	/**
 	 * Cette méthode permet de récupérer une valeur vrai ou fausse, si le pseudo de l'utilisateur existe
 	 * @param pseudo
 	 * @return vrai ou faux
 	 */
-	public boolean findByPseudo(String pseudo);
+	public String findByPseudo(String pseudo);
 }

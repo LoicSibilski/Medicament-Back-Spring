9e50d0444e5d71aacbb422047745c2aad52ef698
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-02 04:07:03 +0200

    feat(CompteServiceimplementation): mise à jour des méthodes

--- src/main/java/com/m2i/medic/compte/services/implementations/ModificationCompteServiceImplementation.java
+++ src/main/java/com/m2i/medic/compte/services/implementations/ModificationCompteServiceImplementation.java
@@ -1,14 +1,24 @@
 package com.m2i.medic.compte.services.implementations;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.m2i.medic.compte.dtos.DesactivationCompteDTO;
 import com.m2i.medic.compte.dtos.InscriptionDTO;
 import com.m2i.medic.compte.dtos.ModificationCompteDTO;
+import com.m2i.medic.compte.entities.Compte;
 import com.m2i.medic.compte.repositories.ModificateurCompteRepository;
 import com.m2i.medic.compte.services.ModificateurCompteService;
 
 /**
- * Cette classe représente l'implémentation du service de modification d'un compte
+ * Cette classe représente l'implémentation du service de modification d'un
+ * compte
+ * 
  * @author fabien
  *
  */
@@ -20,6 +30,7 @@ public class ModificationCompteServiceImplementation implements ModificateurComp
 
 	/**
 	 * Constructeur
+	 * 
 	 * @param mapper
 	 * @param repository
 	 */
@@ -31,23 +42,85 @@ public class ModificationCompteServiceImplementation implements ModificateurComp
 
 	@Override
 	public void creerCompte(InscriptionDTO nouveauCompte) {
-		
+		verifierCompte(nouveauCompte);
+		Compte compte = this.mapper.convertValue(nouveauCompte, Compte.class);
+		compte.setDateCreation(LocalDateTime.now());
+		compte.setDateMisJour(LocalDateTime.now());
+		this.repository.save(compte);
+	}
+
+	/**
+	 * Cette méthode permet de vérifier la conformité d'un nouveau compte
+	 * 
+	 * @param nouveauCompte
+	 */
+	private void verifierCompte(InscriptionDTO nouveauCompte) {
+		String email = this.repository.findByEmail(nouveauCompte.getEmail());
+		String pseudo = this.repository.findByPseudo(nouveauCompte.getPseudo());
+
+		boolean emailExiste = email == null ? false : true;
+		boolean pseudoExiste = pseudo == null ? false : true;
+
+		boolean emailFormatValide = verifierFormatEmail(nouveauCompte.getEmail());
+		boolean pseudoFormatValide = verifierFormatPseudo(nouveauCompte.getPseudo());
+		boolean motDePasseFormatValide = verifierFormatMotDePasse(nouveauCompte.getMotDePasse());
+
+		if (!emailFormatValide || !pseudoFormatValide || !motDePasseFormatValide || emailExiste || pseudoExiste)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+	}
+
+	/**
+	 * Cette méthode permet de vérifier que le format de l'email correspond à la norme RFC 5322
+	 * 
+	 * @param email
+	 * @return vrai ou faux
+	 */
+	private boolean verifierFormatEmail(String email) {
+		Pattern pattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
+				+ "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])"
+				+ "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]"
+				+ "|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]"
+				+ ":(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+		return pattern.matcher(email).matches();
+	}
+
+	/**
+	 * Cette méthode permet de vérifier le format du pseudo
+	 * 
+	 * @param pseudo
+	 * @return vrai ou faux
+	 */
+	private boolean verifierFormatPseudo(String pseudo) {
+		Pattern pattern = Pattern.compile("^([a-zA-Z0-9-_]{2,36})$");
+		return pattern.matcher(pseudo).matches();
+	}
+
+	/**
+	 * Cette méthode permet de vérifier le format du mot de passe
+	 * 
+	 * @param motDePasse
+	 * @return vrai ou faux
+	 */
+	private boolean verifierFormatMotDePasse(String motDePasse) {
+		Pattern pattern = Pattern
+				.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
+		return pattern.matcher(motDePasse).matches();
 	}
 
 	@Override
 	public void modifierCompte(ModificationCompteDTO compteModifie) {
-		
+
 	}
 
 	@Override
 	public void desactiverCompte(DesactivationCompteDTO compteDesactive) {
-		
+
 	}
-	
+
 	/**
 	 * Cette méthode permet de supprimer un compte
 	 */
 	private void supprimerCompte() {
-		
+
 	}
 }

0e336b80ae8c3ee9c4e373dda1bc1971b546d019
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-06 00:52:54 +0200

    feat(modification compte config): ajout d'un bean

--- src/main/java/com/m2i/medic/compte/configs/ModificationCompteConfig.java
+++ src/main/java/com/m2i/medic/compte/configs/ModificationCompteConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.m2i.medic.compte.repositories.ModificateurCompteRepository;
 import com.m2i.medic.compte.services.implementations.ModificationCompteServiceImplementation;
+import com.m2i.medic.compte.services.implementations.VerificationCompteServiceImplementation;
 
 /**
  * Cette classe représente la configuration de la modification d'un compte
@@ -19,10 +20,22 @@ public class ModificationCompteConfig {
 	 * Cette méthode permet de retourner un bean pour utiliser le service de modification d'un compte
 	 * @param mapper
 	 * @param repository
+	 * @param verificateur
 	 * @return un bean
 	 */
 	@Bean
-	public ModificationCompteServiceImplementation creerBeanModificationCompteServiceImplementation(ObjectMapper mapper, ModificateurCompteRepository repository) {
-		return new ModificationCompteServiceImplementation(mapper, repository);
+	public ModificationCompteServiceImplementation creerBeanModificationCompteServiceImplementation(ObjectMapper mapper, ModificateurCompteRepository repository, VerificationCompteServiceImplementation verificateur) {
+		return new ModificationCompteServiceImplementation(mapper, repository, verificateur);
+	}
+	
+	/**
+	 * Cette méthode permet de retourner un bean pour utiliser le service de verification d'un compte
+	 * @param mapper
+	 * @param repository
+	 * @return
+	 */
+	@Bean
+	public VerificationCompteServiceImplementation creerBeanVerificationCompteServiceImplementation(ObjectMapper mapper, ModificateurCompteRepository repository) {
+		return new VerificationCompteServiceImplementation(repository, mapper);
 	}
 }

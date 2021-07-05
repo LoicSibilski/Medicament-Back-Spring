03fb2a83501d029cea7eee590e8682bebfebe411
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-06 01:35:05 +0200

    feat(authentification compte repository): ajout d'une annotation @requestbody

--- src/main/java/com/m2i/medic/compte/controllers/AuthentificationCompteController.java
+++ src/main/java/com/m2i/medic/compte/controllers/AuthentificationCompteController.java
@@ -3,6 +3,7 @@ package com.m2i.medic.compte.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +15,9 @@ import com.m2i.medic.compte.services.AuthentificateurCompteService;
  * @author fabien
  *
  */
+@CrossOrigin
 @RestController
 @RequestMapping("auth")
-@CrossOrigin
 public class AuthentificationCompteController {
 
 	@Autowired
@@ -28,7 +29,7 @@ public class AuthentificationCompteController {
 	 * @return l'identifiant du compte
 	 */
 	@PostMapping("connexion")
-	public String seConnecterCompte(ConnexionDTO connexion) {
-		return this.service.seConnecterCompte(connexion);
+	public String seConnecterCompte(@RequestBody ConnexionDTO compteConnexion) {
+		return this.service.seConnecterCompte(compteConnexion);
 	}
 }

91a2c5f3a9f85061a1d0e945b8859c0ea022fdaf
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-06 00:52:32 +0200

    feat(modification compte service): mise à jour des méthodes

--- src/main/java/com/m2i/medic/compte/services/implementations/ModificationCompteServiceImplementation.java
+++ src/main/java/com/m2i/medic/compte/services/implementations/ModificationCompteServiceImplementation.java
@@ -1,11 +1,8 @@
 package com.m2i.medic.compte.services.implementations;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
-import java.util.regex.Pattern;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.m2i.medic.compte.dtos.DesactivationCompteDTO;
@@ -27,22 +24,24 @@ public class ModificationCompteServiceImplementation implements ModificateurComp
 	private ObjectMapper mapper;
 
 	private ModificateurCompteRepository repository;
-
+	
+	private VerificationCompteServiceImplementation verificateur;
+		
 	/**
 	 * Constructeur
 	 * 
 	 * @param mapper
 	 * @param repository
 	 */
-	public ModificationCompteServiceImplementation(ObjectMapper mapper, ModificateurCompteRepository repository) {
-		super();
+	public ModificationCompteServiceImplementation(ObjectMapper mapper, ModificateurCompteRepository repository, VerificationCompteServiceImplementation verificateur) {
 		this.mapper = mapper;
 		this.repository = repository;
+		this.verificateur = verificateur;
 	}
 
 	@Override
 	public void creerCompte(InscriptionDTO nouveauCompte) {
-		verifierCompte(nouveauCompte);
+		this.verificateur.verifierDonneesCompte(nouveauCompte);
 		Compte compte = this.mapper.convertValue(nouveauCompte, Compte.class);
 		compte.setDateCreation(LocalDateTime.now());
 		compte.setEtat(true);
@@ -51,112 +50,34 @@ public class ModificationCompteServiceImplementation implements ModificateurComp
 
 	@Override
 	public void modifierCompte(ModificationCompteDTO compteModifie) {
-		
-		Optional<Compte> optionnel = this.repository.findById(compteModifie.getId());
-		Compte nouveauCompte = optionnel.orElseThrow(()-> {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-		});
-		
-		if(!compteModifie.getEmail().isEmpty() && compteModifie.getEmail() != nouveauCompte.getEmail()) {
-			verifierEmail(compteModifie);			
-			nouveauCompte.setEmail(compteModifie.getEmail());
-		}
-		
-		if(!compteModifie.getPseudo().isEmpty() && compteModifie.getPseudo() != nouveauCompte.getPseudo()) {
-			verifierPseudo(compteModifie);			
-			nouveauCompte.setPseudo(compteModifie.getPseudo());
-		}
-		
-		if(!compteModifie.getMotDePasse().isEmpty() && compteModifie.getMotDePasse() != nouveauCompte.getMotDePasse()) {
-			nouveauCompte.setMotDePasse(compteModifie.getMotDePasse());
-		}
-		
+		this.verificateur.verifierDonneesCompte(compteModifie);
+		Compte nouveauCompte = this.mapper.convertValue(compteModifie, Compte.class);
+		nouveauCompte.setEmail(compteModifie.getEmail());
+		nouveauCompte.setPseudo(compteModifie.getPseudo());
+		nouveauCompte.setMotDePasse(compteModifie.getMotDePasse());
 		nouveauCompte.setDateMisJour(LocalDateTime.now());
+		nouveauCompte.setEtat(true);
 		this.repository.save(nouveauCompte);
 	}	
 
 	@Override
 	public void desactiverCompte(DesactivationCompteDTO compteDesactive) {
-
+		this.verificateur.verifierDonneesCompte(compteDesactive);
+		Optional<Compte> compte = this.repository.findById(compteDesactive.getId());
+		Compte compteSuspendu = this.mapper.convertValue(compte, Compte.class);
+		compteSuspendu.setEmail(compte.get().getEmail());
+		compteSuspendu.setPseudo(compte.get().getPseudo());
+		compteSuspendu.setMotDePasse(compte.get().getMotDePasse());
+		compteSuspendu.setDateMisJour(LocalDateTime.now());
+		compteSuspendu.setEtat(false);
+		supprimerCompte(compte.get().getId());
+		this.repository.save(compteSuspendu);
 	}
 
 	/**
 	 * Cette méthode permet de supprimer un compte
 	 */
-	private void supprimerCompte() {
-
+	private void supprimerCompte(String id) {
+		this.repository.deleteById(id);
 	}
-
-	/**
-	 * Cette méthode permet de vérifier la conformité d'un nouveau compte
-	 * 
-	 * @param nouveauCompte
-	 */
-	private void verifierCompte(InscriptionDTO nouveauCompte) {
-		boolean emailExiste = this.repository.findByEmail(nouveauCompte.getEmail()) == null ? false : true;
-		boolean emailFormatValide = verifierFormatEmail(nouveauCompte.getEmail());
-		
-		boolean pseudoExiste = this.repository.findByPseudo(nouveauCompte.getPseudo()) == null ? false : true;		
-		boolean pseudoFormatValide = verifierFormatPseudo(nouveauCompte.getPseudo());
-		
-		boolean motDePasseFormatValide = verifierFormatMotDePasse(nouveauCompte.getMotDePasse());
-
-		if (!emailFormatValide || !pseudoFormatValide || !motDePasseFormatValide || emailExiste || pseudoExiste)
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-	}
-	
-	
-	public void verifierEmail(ModificationCompteDTO nouveauCompte) {
-		boolean emailExiste = this.repository.findByEmail(nouveauCompte.getEmail()) == null ? false : true;
-		boolean emailFormatValide = verifierFormatEmail(nouveauCompte.getEmail());
-		if (!emailFormatValide ||  emailExiste)
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-	}
-	
-	public void verifierPseudo(ModificationCompteDTO nouveauCompte) {
-		boolean pseudoExiste = this.repository.findByPseudo(nouveauCompte.getPseudo()) == null ? false : true;		
-		boolean pseudoFormatValide = verifierFormatPseudo(nouveauCompte.getPseudo());
-		if (!pseudoFormatValide || pseudoExiste)
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-	}
-
-	/**
-	 * Cette méthode permet de vérifier que le format de l'email correspond à la
-	 * norme RFC 5322
-	 * 
-	 * @param email
-	 * @return vrai ou faux
-	 */
-	private boolean verifierFormatEmail(String email) {
-		Pattern pattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\""
-				+ "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])"
-				+ "*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]"
-				+ "|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]"
-				+ ":(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-		return pattern.matcher(email).matches();
-	}
-
-	/**
-	 * Cette méthode permet de vérifier le format du pseudo
-	 * 
-	 * @param pseudo
-	 * @return vrai ou faux
-	 */
-	private boolean verifierFormatPseudo(String pseudo) {
-		Pattern pattern = Pattern.compile("^([a-zA-Z0-9-_]{2,36})$");
-		return pattern.matcher(pseudo).matches();
-	}
-
-	/**
-	 * Cette méthode permet de vérifier le format du mot de passe
-	 * 
-	 * @param motDePasse
-	 * @return vrai ou faux
-	 */
-	private boolean verifierFormatMotDePasse(String motDePasse) {
-		Pattern pattern = Pattern
-				.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
-		return pattern.matcher(motDePasse).matches();
-	}
-
 }

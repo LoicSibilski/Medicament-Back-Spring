adb4962fe42b4e596308b935215824190b33eb27
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-07-15 21:49:00 +0200

    feat(notification): mise à jour

---
+++ src/main/java/com/m2i/medic/notification/controllers/NotificationController.java
@@ -0,0 +1,45 @@
+package com.m2i.medic.notification.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.m2i.medic.notification.dtos.NotificationDTO;
+import com.m2i.medic.notification.entities.Notification;
+import com.m2i.medic.notification.services.NotificateurService;
+
+/**
+ * Cette classe représent un contrôleur de notifications
+ * @author fabien
+ *
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("notifications")
+public class NotificationController {
+	
+	@Autowired
+	private NotificateurService service;
+	
+	/**
+	 * Cette méthode permet de créer une notification à partir du service
+	 * @param notification
+	 */
+	@PostMapping("")
+	public void creerNotification(@RequestBody NotificationDTO notification) {
+		this.service.creerNotification(notification);
+	}
+	
+	@GetMapping("/{utilisateurId}")
+	public List<Notification> recupererNotificationsUtilisateur(@PathVariable String utilisateurId) {
+		return this.service.recupererNotificationsUtilisateur(utilisateurId);
+	}
+	
+}
